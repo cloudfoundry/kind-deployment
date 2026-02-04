1cdd351ad9e32261701bdf59214fb819c4817a77
Nicolas Bender <nicolas.bender@sap.com>
2026-02-04 15:59:55 +0100

    Fix again
    Co-authored-by: Ralf Pannemans <ralf.pannemans@sap.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -59,7 +59,7 @@ helm upgrade --install route-emitter releases/diego/helm --set "routeEmitter.ena
 helm upgrade --install k8s-rep oci://ghcr.io/cloudfoundry/helm/k8s-rep:$K8S_REP_VERSION --set-file ca_crt="$CERTS_DIR/ca.crt" --set "stacks.cflinuxfs4=ghcr.io/cloudfoundry/k8s/cflinuxfs4:1.300.0"
 helm upgrade --install api releases/capi/helm --set blobstorePassword=$BLOBSTORE_PASSWORD --set dbPassword=$DB_PASSWORD --set oauthClientsSecret=$OAUTH_CLIENTS_SECRET --set cloudController.sshProxyKeyFingerprint=$SSH_PROXY_KEY_FINGERPRINT --set "blobstore.enabled=true" --set "cloudController.enabled=true" --wait
 helm upgrade --install cf-networking releases/cf-networking/helm --set policyServer.dbPassword=$DB_PASSWORD --set policyServer.oauthClientsSecret=$OAUTH_CLIENTS_SECRET
-helm upgrade --install policy-agent oci://ghcr.io/cloudfoundry/policy-agent:$POLICY_AGENT_VERSION
+helm upgrade --install policy-agent oci://ghcr.io/cloudfoundry/helm/policy-agent:$POLICY_AGENT_VERSION
 
 kubectl get configmap coredns -n kube-system -o jsonpath='{.data.Corefile}' | grep -q "apps.internal:53" || {
   echo "Patching CoreDNS to forward apps.internal to bosh-dns"
