d42c49320d4b95e14d34e77fab292798fd55a583
Nicolas Bender <nicolas.bender@sap.com>
2026-02-06 13:08:01 +0100

    Use gitbot author from secret

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,6 +1,5 @@
 module.exports = {
     username: 'ard-wg-k8s-gitbot',
-    gitAuthor: 'ard-wg-k8s-gitbot <kind-deployment@cloudfoundry.org>',
     onboarding: false,
     platform: 'github',
     repositories: [
