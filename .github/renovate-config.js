7f2408305d868ccf789912614dd3131adeb2a700
Nicolas Bender <nicolas.bender@sap.com>
2026-02-06 12:55:28 +0100

    Use gitbot as workflow author

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,4 +1,6 @@
 module.exports = {
+    username: 'ard-wg-k8s-gitbot',
+    gitAuthor: 'ard-wg-k8s-gitbot <kind-deployment@cloudfoundry.org>',
     onboarding: false,
     platform: 'github',
     repositories: [
