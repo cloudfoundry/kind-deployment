dd6ba91bf71776fe4e2b85d5318d6a6bbab9d78b
Matthias Vach <matthias.vach@gmail.com>
2026-02-12 13:56:18 +0100

    make bootstrap idempotent (#81)
    Co-authored-by: Nicolas Bender <nicolas.bender@sap.com>

--- scripts/upload_buildpacks.sh
+++ scripts/upload_buildpacks.sh
@@ -2,7 +2,28 @@
 
 set -e
 
-cf create-buildpack java_buildpack http://file-server.127-0-0-1.nip.io/java-buildpack-cflinuxfs4.zip 2
-cf create-buildpack nodejs_buildpack http://file-server.127-0-0-1.nip.io/nodejs_buildpack-cflinuxfs4.zip 5
-cf create-buildpack go_buildpack http://file-server.127-0-0-1.nip.io/go_buildpack-cflinuxfs4.zip 6
-cf create-buildpack binary_buildpack http://file-server.127-0-0-1.nip.io/binary_buildpack-cflinuxfs4.zip 11
+INSTALLED_BUILPACKS=$(cf curl /v3/buildpacks | jq '.resources[].name')
+
+if [[ $INSTALLED_BUILPACKS == *"java_buildpack"* ]]; then
+  echo "java_buildpack already exists, skipping upload"
+else
+  cf create-buildpack java_buildpack http://file-server.127-0-0-1.nip.io/java-buildpack-cflinuxfs4.zip 1
+fi
+
+if [[ $INSTALLED_BUILPACKS == *"nodejs_buildpack"* ]]; then
+  echo "nodejs_buildpack already exists, skipping upload"
+else
+  cf create-buildpack nodejs_buildpack http://file-server.127-0-0-1.nip.io/nodejs_buildpack-cflinuxfs4.zip 2
+fi
+
+if [[ $INSTALLED_BUILPACKS == *"go_buildpack"* ]]; then
+  echo "go_buildpack already exists, skipping upload"
+else
+  cf create-buildpack go_buildpack http://file-server.127-0-0-1.nip.io/go_buildpack-cflinuxfs4.zip 3
+fi
+
+if [[ $INSTALLED_BUILPACKS == *"binary_buildpack"* ]]; then
+  echo "binary_buildpack already exists, skipping upload"
+else
+  cf create-buildpack binary_buildpack http://file-server.127-0-0-1.nip.io/binary_buildpack-cflinuxfs4.zip 4
+fi
