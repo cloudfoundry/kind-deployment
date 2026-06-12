4ee7edf8dc5c6d6b406bc83417ab4d0ed7ebf553
Ralf Pannemans <ralf.pannemans@sap.com>
2026-06-12 13:20:06 +0200

    Consume fileserver with mounting buildpacks (#432)
    * Use fileserver with buildpacks mount
    
    Co-authored-by: Ralf Pannemans <ralf.pannemans@sap.com>
    
    * Adjust buildpack yaml structure
    
    Co-authored-by: Nicolas Bender <nicolas.bender@sap.com>
    
    * Update sync-cf-deployment script with buildpacks
    
    * Buildpack renaming
    
    Co-authored-by: Ralf Pannemans <ralf.pannemans@sap.com>
    
    * Use upstream diego
    
    Co-authored-by: Nicolas Bender <nicolas.bender@sap.com>
    
    * Remove comment
    
    Co-authored-by: Ralf Pannemans <ralf.pannemans@sap.com>
    
    * Use correct version
    
    Co-authored-by: Nicolas Bender <nicolas.bender@sap.com>
    
    * Use underscores in all buildpack names
    
    Co-authored-by: Ralf Pannemans <ralf.pannemans@sap.com>
    Co-authored-by: Nicolas Bender <nicolas.bender@sap.com>
    
    ---------
    
    Co-authored-by: Nicolas Bender <nicolas.bender@sap.com>

--- scripts/upload_buildpacks.sh
+++ scripts/upload_buildpacks.sh
@@ -2,21 +2,21 @@
 
 set -e
 
-INSTALLED_BUILPACKS=$(cf curl /v3/buildpacks | jq -r '.resources[] | "#" + .name + "#"')
+INSTALLED_BUILDPACKS=$(cf curl /v3/buildpacks | jq -r '.resources[] | "#" + .name + "#"')
 
-buildpacks=("java_buildpack" "nodejs_buildpack" "go_buildpack" "binary_buildpack")
+buildpacks=("java-buildpack" "nodejs-buildpack" "go-buildpack" "binary-buildpack")
 position=1
 
 if [[ $ALL_BUILDPACKS == "true" ]]; then
-  buildpacks+=("dotnet-core_buildpack" "nginx_buildpack" "php_buildpack" "python_buildpack" "r_buildpack" "ruby_buildpack" "staticfile_buildpack")
+  buildpacks+=("dotnet-core-buildpack" "nginx-buildpack" "php-buildpack" "python-buildpack" "r-buildpack" "ruby-buildpack" "staticfile-buildpack")
 fi
 
-
 for buildpack in "${buildpacks[@]}"; do
-  if [[ $INSTALLED_BUILPACKS =~ "#$buildpack#" ]]; then
-    cf update-buildpack "$buildpack" -p "http://fileserver.127-0-0-1.nip.io/${buildpack}-cflinuxfs4.zip"
+  buildpack_name=$(echo "$buildpack" | sed 's/-buildpack/_buildpack/')
+  if [[ $INSTALLED_BUILDPACKS =~ "#$buildpack_name#" ]]; then
+    cf update-buildpack "$buildpack_name" -p "http://fileserver.127-0-0-1.nip.io/${buildpack}/${buildpack}.zip"
   else
-    cf create-buildpack "$buildpack" "http://fileserver.127-0-0-1.nip.io/${buildpack}-cflinuxfs4.zip" "$position"
+    cf create-buildpack "$buildpack_name" "http://fileserver.127-0-0-1.nip.io/${buildpack}/${buildpack}.zip" "$position"
   fi
   ((position++))
 done
