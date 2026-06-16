f3cf95dd2333ea1dcc163d9fccc986ef3bf51aa2
Johannes Dillmann <j.dillmann@sap.com>
2026-06-16 15:44:02 +0200

    aadd fallback url

--- scripts/upload_buildpacks.sh
+++ scripts/upload_buildpacks.sh
@@ -15,10 +15,17 @@ for buildpack in "${buildpacks[@]}"; do
   buildpack_name=$(echo "$buildpack" | sed 's/-buildpack/_buildpack/')
   buildpack_version=$(yq e ".buildpacks.${buildpack}.tag" "versions.yaml")
   buildpack_zip="${buildpack}-cflinuxfs4-v${buildpack_version}.zip"
+  buildpack_url="http://fileserver.127-0-0-1.nip.io/${buildpack}/${buildpack_zip}"
+  fallback_buildpack_url="http://fileserver.127-0-0-1.nip.io/${buildpack}/${buildpack}.zip"
+
+  if ! curl -fsSLI "$buildpack_url" >/dev/null 2>&1; then
+    buildpack_url="$fallback_buildpack_url"
+  fi
+
   if [[ $INSTALLED_BUILDPACKS =~ "#$buildpack_name#" ]]; then
-    cf update-buildpack "$buildpack_name" -p "http://fileserver.127-0-0-1.nip.io/${buildpack}/${buildpack_zip}"
+    cf update-buildpack "$buildpack_name" -p "$buildpack_url"
   else
-    cf create-buildpack "$buildpack_name" "http://fileserver.127-0-0-1.nip.io/${buildpack}/${buildpack_zip}" "$position"
+    cf create-buildpack "$buildpack_name" "$buildpack_url" "$position"
   fi
   ((position++))
 done
