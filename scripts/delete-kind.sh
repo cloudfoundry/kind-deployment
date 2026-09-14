fb845c8410868d31ba06f912d68756b86073c2ef
Pascal Zimmermann <pascal.zimmermann01@sap.com>
2026-09-14 09:24:39 +0200

    feat: Add Podman support as drop-in replacement for Docker     (#360)

--- scripts/delete-kind.sh
+++ scripts/delete-kind.sh
@@ -10,7 +10,14 @@ tools::install::kind
 
 kind delete cluster --name cfk8s
 
+# Detect container runtime behind docker (Docker vs Podman)
+if echo $(docker version) | grep -qi 'Podman'; then
+  container_runtime_spects=""
+else
+  container_runtime_spects="--progress plain"
+fi
+
 # Remove registry cache containers using docker-compose
 echo "Deleting registry cache containers..."
-docker compose -p cache -f "${script_full_path}/docker-compose-registries.yaml" --progress plain down
-docker compose -p nfs -f "${script_full_path}/docker-compose-nfs.yaml" --progress plain down
+docker compose -p cache -f "${script_full_path}/docker-compose-registries.yaml" $container_runtime_spects down
+docker compose -p nfs -f "${script_full_path}/docker-compose-nfs.yaml" $container_runtime_spects down
